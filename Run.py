a403cd1ed830e206b62274bc82020a328d581704
c_youwu <c_youwu@qti.qualcomm.com>
2017-09-06 16:49:16 +0800

    add

--- Run.py
+++
@@ -1,9 +0,0 @@
-from libs.ui.Client import Client
-import wx
-
-
-if __name__ == '__main__':
-    app = wx.App()
-    client = Client()
-    client.Show()
-    app.MainLoop()
--- libs/ui/tmp.py
+++
@@ -1,8 +0,0 @@
-with open ('C:\Users\c_youwu\Desktop\ddd.txt') as r:
-    for line in r.readlines():
-        line = line.strip('\r\n').lstrip(' ')
-        #print line
-        profile = line[ line.index('(profile'):].replace('(profile.get(', 'profile[').replace('(ic.name)))','(ic.name)]')
-        v = line[ :line.index('(profile')].replace('SetStringSelection', 'GetStringSelection').replace('SetValue','GetValue')+'()'
-        print profile +' = '+v
-
